83e089e4c512588ba180133cdb0764544a8dfde7
Surfoo <surfooo@gmail.com>
2014-08-08 11:31:01 +0200

    Script updated

--- monitoring.sh
+++ monitoring.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 
-BASEDIR=$(dirname $0)
-cd $BASEDIR;
+cd $(dirname $0);
 
 wget -m -p -E -k -q -np https://staging.api.groundspeak.com/Live/v6beta/geocaching.svc/help;
 
-if [[ $(git ls-files -m | wc -l) -gt 0 ]];then
-    FILES=$(git ls-files -m);
+if [[ $(git ls-files -m | grep staging.api.groundspeak.com | wc -l) -gt 0 ]];then
+    FILES=$(git ls-files -m | grep staging.api.groundspeak.com);
+    echo "Files found:
+$FILES";
     git commit -am "Changes detected on:
 $FILES";
     git push;
