fc7f70edf22c8f22c24395e14726ce17f135c16b
root <root@ks361529.kimsufi.com>
2014-12-02 08:17:06 +0100

    Changes detected on: staging.api.groundspeak.com/Live/v6beta/geocaching.svc/help/operations/SaveUserWaypoint.html staging.api.groundspeak.com/Live/v6beta/geocaching.svc/help/operations/SearchForGeocaches.html staging.api.groundspeak.com/Live/v6beta/geocaching.svc/help/operations/WindowsPhoneTileSearch.html wsdl_staging.xml

--- monitoring.php
+++ monitoring.php
@@ -5,9 +5,12 @@ $wsdl_url = ['staging' => 'https://staging.api.groundspeak.com/Live/V6Beta/geoca
              'live'    => 'https://api.groundspeak.com/LiveV6/geocaching.svc?singleWsdl',
             ];
 
+echo "WSDL:\n";
 foreach ($wsdl_url as $platform => $url) {
+    echo $platform . "\n"; 
     $xml = new \DOMDocument();
     $xml->formatOutput = true;
     $xml->loadXML(file_get_contents($url));
     file_put_contents('wsdl_' . $platform . '.xml', $xml->saveXML());
 }
+echo "Check WSQL, done.\n";
