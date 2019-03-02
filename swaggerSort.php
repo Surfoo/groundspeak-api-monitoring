5b45e114dc76c809b942e98f9a985b9b94bbc654
Surfoo <surfooo@gmail.com>
2019-03-02 11:13:48 +0100

    Changes detected on: production/swagger.json staging/swagger.json

--- swaggerSort.php
+++ swaggerSort.php
@@ -2,12 +2,16 @@
 
 unset($argv[0]);
 
+$keysToSort = ['type', 'logType', 'size', 'status'];
+
 foreach($argv as $file) {
     $contentFile = json_decode(file_get_contents($file), true);
 
     foreach ($contentFile['definitions'] as $key => &$content) {
-        if (isset($content['properties']['type']) && isset($content['properties']['type']['enum'])) {
-            sort($content['properties']['type']['enum']);
+        foreach ($keysToSort as $keyToSort) {
+            if (isset($content['properties'][$keyToSort]) && isset($content['properties'][$keyToSort]['enum'])) {
+                sort($content['properties'][$keyToSort]['enum']);
+            }
         }
     }
 
