bc35742d56c017ad8b1dce77c1ef745fb9fb9aea
Surfoo <surfooo@gmail.com>
2019-08-21 10:39:04 +0200

    Fix bug on the list of modified files

--- monitoring.sh
+++ monitoring.sh
@@ -10,15 +10,13 @@ wget -e robots=off -m -E -q -A.html "https://staging.api.groundspeak.com/api-doc
 wget -e robots=off -m -E -q -A.html "https://api.groundspeak.com/documentation" -O "production/documentation.html"
 wget -e robots=off -m -E -q -A.html "https://api.groundspeak.com/api-docs/v1/swagger" -O "production/swagger.json"
 
+SWAGGER_FILES=$(git ls-files -m | grep -E 'swagger.json');
+php swaggerSort.php $SWAGGER_FILES
+
 if [[ $(git ls-files -m | grep -E 'staging|production' | wc -l) -gt 0 ]];then
     FILES=$(git ls-files -m | grep -E 'staging|production');
-    SWAGGER_FILES=$(git ls-files -m | grep -E 'swagger.json');
     printf "Files found:\n%s\n\n" "$FILES";
-    if [[ $SWAGGER_FILES ]];then
-        php swaggerSort.php $SWAGGER_FILES
-    fi
-    git commit --author "Surfoo <surfooo@gmail.com>" -am "Changes detected on:
-$FILES" && git push;
+    git commit --author "Surfoo <surfooo@gmail.com>" -am $'Changes detected on:\n$FILES' && git push;
 else
     printf 'Nothing to do.\n\n';
 fi
